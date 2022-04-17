86ac2ca7c4f2eec0d83a7f925dfd3741c9f0fd7e
Bassam <muhammadbassam73@gmail.com>
2022-04-17 22:47:48 +0500

    del

--- Course/food/src/api/yelp.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export default axios.create({
-    baseURL: 'https://api.yelp.com/v3/businesses',
-    headers:{
-        Authorization: 'Bearer zHYZmnzCPvmYe4WHEWOaaLzoWsYta5FErkzDMKMMlnZLT8KI9S-JXOAOF2X4SIADFN9ZMCKwegrK2O0lnBhBT6kF605lXbmsU6K2p7RC7FukQAGrIt2-q7RG6vlTYnYx'
-    }
-})
